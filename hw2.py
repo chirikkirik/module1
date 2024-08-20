613376687a153bfa120b09c96de9b659a41b0430
chirikkirik <149368902+chirikkirik@users.noreply.github.com>
2024-08-20 14:26:46 +0300

    Add files via upload
    Практическое задание по работе в Pycharm - "Переменные".

---
+++ hw2.py
@@ -0,0 +1,6 @@
+numberOfCompletedHomeworks = 12
+numberOfHoursSpent = 1.5
+courseName = 'Python'
+timeForOneTask = numberOfHoursSpent / numberOfCompletedHomeworks
+print('Курс:', courseName, ',', 'всего задач:', numberOfCompletedHomeworks,
+      ',', 'затрачено часов:', numberOfHoursSpent, ',', 'среднее время выполнения', timeForOneTask, 'wчаса.')
